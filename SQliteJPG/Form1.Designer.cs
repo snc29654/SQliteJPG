e86b3f568f6820c715c6eb4da5b0bff21bbc041f
snc29654 <snc29654@nifty.com>
2022-03-26 19:38:12 +0900

    検索読み出しを追加

--- SQliteJPG/Form1.Designer.cs
+++ SQliteJPG/Form1.Designer.cs
@@ -36,6 +36,10 @@ namespace SQliteJPG
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -74,7 +78,7 @@ namespace SQliteJPG
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(34, 233);
+            this.textBox1.Location = new System.Drawing.Point(38, 312);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
@@ -83,26 +87,65 @@ namespace SQliteJPG
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(312, 46);
+            this.textBox2.Location = new System.Drawing.Point(359, 55);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(116, 27);
             this.textBox2.TabIndex = 4;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(87, 176);
+            this.button3.Location = new System.Drawing.Point(88, 180);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(177, 51);
             this.button3.TabIndex = 5;
-            this.button3.Text = "DB読み出し";
+            this.button3.Text = "DB検索読み出し";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "写真名";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(359, 188);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(108, 27);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "NO";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(88, 249);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(177, 44);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "DB全読み出し";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1312, 636);
+            this.ClientSize = new System.Drawing.Size(1312, 711);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -126,6 +169,10 @@ namespace SQliteJPG
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- SQliteJPG/Form1.cs
+++ SQliteJPG/Form1.cs
@@ -60,7 +60,7 @@ namespace SQliteJPG
                 string sql = "";
                 sql += "CREATE TABLE IF NOT EXISTS sample ";
                 sql += "( ";
-                sql += "  no INTEGER NOT NULL, ";
+                sql += "  no INTEGER PRIMARY KEY AUTOINCREMENT, ";
                 sql += "  title TEXT, ";
                 sql += "  file_binary BLOB ";
                 sql += ") ";
@@ -75,7 +75,7 @@ namespace SQliteJPG
                 byte[] file_binary_from = File.ReadAllBytes(pathname);
 
                 // データを挿入する
-                sql = $" INSERT INTO sample(no,title,file_binary) VALUES (1,@name, @file_binary)";
+                sql = $" INSERT INTO sample(no,title,file_binary) VALUES (NULL,@name, @file_binary)";
                 cmd.CommandText = sql;
                 cmd.Parameters.Add(new SQLiteParameter("@name", textBox2.Text));
                 cmd.Parameters.Add("@file_binary", DbType.Binary).Value = file_binary_from;
@@ -161,9 +161,9 @@ namespace SQliteJPG
                 //cmd.ExecuteNonQuery();
                 // ファイルをバイト配列に変換する
 
-
+                int number = int.Parse(textBox3.Text);
                 // データを取得する
-                sql = $" SELECT * FROM sample ";
+                sql = $" SELECT * FROM sample WHERE no = " + number;
                 cmd.CommandText = sql;
                 SQLiteDataReader reader = cmd.ExecuteReader();
                 while (reader.Read() == true)
@@ -191,6 +191,83 @@ namespace SQliteJPG
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.Image = image;
             con.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+            // EXEの起動パスを取得する
+            string exePath = System.Windows.Forms.Application.StartupPath;
+
+            // DBフルパスを組みたてる
+            string dbFullPath = System.IO.Path.Combine(exePath, "test.db");
+
+            // 接続先データベースを指定する
+            SQLiteConnection con = new SQLiteConnection(String.Format($"Data Source = {dbFullPath}"));
+
+            DataTable dt = new DataTable();
+
+            DateTime date = DateTime.Now;
+
+            string jpgname = date.ToString("yyyyMMddHHmmss");
+
+            try
+            {
+                // データベースと接続する
+                con.Open();
+
+                // SQLコマンドを宣言する
+                SQLiteCommand cmd = con.CreateCommand();
+
+                // テーブルを作成する
+                string sql = "";
+                sql += "CREATE TABLE IF NOT EXISTS sample ";
+                sql += "( ";
+                sql += "  no INTEGER NOT NULL, ";
+                sql += "  title TEXT, ";
+                sql += "  file_binary BLOB ";
+                sql += ") ";
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+
+                // データを全て削除する
+                //sql = "DELETE FROM sample ";
+                //cmd.CommandText = sql;
+                //cmd.ExecuteNonQuery();
+                // ファイルをバイト配列に変換する
+
+                // データを取得する
+                sql = $" SELECT * FROM sample";
+                cmd.CommandText = sql;
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                while (reader.Read() == true)
+                {
+                    textBox1.Text += reader["no"].ToString();
+                    textBox1.Text += " : ";
+                    textBox1.Text += reader["title"].ToString();
+                    textBox1.Text += " : ";
+                    textBox1.Text += "\r\n";
+                    // BLOBのファイルをバイト配列に変換する
+                    byte[] file_binary_to = (byte[])reader["file_binary"];
+
+                    // ファイルに書き出す
+                    File.WriteAllBytes(@"C:\temp\" + jpgname + @".jpg", file_binary_to);
+                }
+
+
+            }
+            finally
+            {
+                // データベースを切断する
+                con.Close();
+            }
+            Bitmap image = new Bitmap(@"C:\temp\" + jpgname + @".jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = image;
+            con.Close();
+
 
         }
     }
