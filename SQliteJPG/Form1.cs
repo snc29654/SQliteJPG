06933ffaeeb7d6d0ec8c8b2d551eccfcdbc88aab
snc29654 <snc29654l@nifty.com>
2022-03-30 12:31:20 +0900

    ReadAll()関数追加

--- SQliteJPG/Form1.cs
+++ SQliteJPG/Form1.cs
@@ -20,92 +20,7 @@ namespace SQliteJPG
         public Form1()
         {
             InitializeComponent();
-            textBox1.Clear();
-
-
-            // EXEの起動パスを取得する
-            string exePath = System.Windows.Forms.Application.StartupPath;
-
-            // DBフルパスを組みたてる
-            string dbFullPath = System.IO.Path.Combine(exePath, "test.db");
-
-            // 接続先データベースを指定する
-            SQLiteConnection con = new SQLiteConnection(String.Format($"Data Source = {dbFullPath}"));
-
-            DataTable dt = new DataTable();
-
-            DateTime date = DateTime.Now;
-
-            string jpgname = date.ToString("yyyyMMddHHmmss");
-
-            try
-            {
-                // データベースと接続する
-                con.Open();
-
-                // SQLコマンドを宣言する
-                SQLiteCommand cmd = con.CreateCommand();
-
-                // テーブルを作成する
-                string sql = "";
-                sql += "CREATE TABLE IF NOT EXISTS sample ";
-                sql += "( ";
-                sql += "  no INTEGER PRIMARY KEY AUTOINCREMENT, ";
-                sql += "  title TEXT, ";
-                sql += "  file_binary BLOB ";
-                sql += ") ";
-                cmd.CommandText = sql;
-                cmd.ExecuteNonQuery();
-
-                // データを全て削除する
-                //sql = "DELETE FROM sample ";
-                //cmd.CommandText = sql;
-                //cmd.ExecuteNonQuery();
-                // ファイルをバイト配列に変換する
-
-                // データを取得する
-                sql = $" SELECT * FROM sample";
-                cmd.CommandText = sql;
-                SQLiteDataReader reader = cmd.ExecuteReader();
-                int count = 0;
-                while (reader.Read() == true)
-                {
-                    count++;
-                    textBox1.Text += reader["no"].ToString();
-                    textBox1.Text += " : ";
-                    textBox1.Text += reader["title"].ToString();
-                    textBox1.Text += " : ";
-                    textBox1.Text += "\r\n";
-                    // BLOBのファイルをバイト配列に変換する
-                    byte[] file_binary_to = (byte[])reader["file_binary"];
-
-                    // ファイルに書き出す
-                    File.WriteAllBytes(@"C:\temp\" + jpgname + @".jpg", file_binary_to);
-                }
-                if (count == 0)
-                {
-
-                    MessageBox.Show("DBが空です");
-                    return;
-
-                }
-
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("DBが空です");
-                return;
-            }
-
-            finally
-            {
-                // データベースを切断する
-                con.Close();
-            }
-            Bitmap image = new Bitmap(@"C:\temp\" + jpgname + @".jpg");
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.Image = image;
-            con.Close();
+            ReadAll();
 
 
         }
@@ -306,6 +221,14 @@ namespace SQliteJPG
 
         private void button4_Click(object sender, EventArgs e)
         {
+
+            ReadAll();
+
+
+        }
+
+        private void ReadAll()
+        {
             textBox1.Clear();
 
 
@@ -395,6 +318,8 @@ namespace SQliteJPG
 
 
         }
+
+
 
         private void button5_Click(object sender, EventArgs e)
         {
